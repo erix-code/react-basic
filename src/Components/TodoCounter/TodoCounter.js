25f9956db346dd02aa84f9fd0a748611617c15cc
gru-erick <ermateze@gmail.com>
2024-05-29 10:14:49 -0400

    - Adding new function to detect if all the tasks are completed.

--- src/Components/TodoCounter/TodoCounter.js
+++ src/Components/TodoCounter/TodoCounter.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./TodoCounter.scss";
 function TodoCounter ({tasksQuantity, completedTasks}) {
+    const [isCompleted, setIsCompleted] = useState(false);
+    const detectComplete = () => {
+        if (tasksQuantity === completedTasks) {
+            setIsCompleted(true);
+            console.log(isCompleted);
+        } else {
+            setIsCompleted(false);
+            console.log(isCompleted);
+        }
+    }
+    useEffect(() => {
+        detectComplete();
+    }, [completedTasks, tasksQuantity]);
     return(
         <>
             <h1 className={"title-counter"}>
                 📔 TODO LIST!
             </h1>
             <h2 className={"text-center"}>
-                Completed tasks {completedTasks} of {tasksQuantity}
+                { isCompleted & tasksQuantity > 0 ? "Great You Have Completed all your tasks 🎉!" : `Completed tasks ${completedTasks} of ${tasksQuantity}` }
             </h2>
+
         </>
 
     )
