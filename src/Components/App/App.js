82ffdac4964d1d71718e0d36921f2ebcd55ae3cb
gru-erick <ermateze@gmail.com>
2024-06-03 16:44:04 -0400

    - Adding TodoProvider to the root <App> - Using context values in App.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
 import {TodoList} from "../TodoList/TodoList";
 import TodoSearch from "../TodoSearch/TodoSearch";
 import TodoCounter from "../TodoCounter/TodoCounter";
 import {CreateTodoButton} from "../CreateTodoButton/CreateTodoButton";
-import useLocalStorageItems from "../../Hooks/UseLocalStorageItems";
 import CustomSpinner from "../../Elements/CustomSpinner/CustomSpinner";
 import ErrorMessage from "../../Elements/Messages/ErrorMessage";
 import CreateTaskMessage from "../../Elements/Messages/CreateTaskMessage";
@@ -21,31 +20,24 @@ import {TodoContext, TodoProvider} from "../../Contexts/TodoContext";
 // ];
 // localStorage.setItem("TASKS_V1", JSON.stringify(defaultTasks));
 // localStorage.removeItem("TASKS_V1");
-
 function App() {
-
+    const { tasks,saveItem, filteredTasks, isLoading, hasError} = useContext(TodoContext);
     return (
-        <TodoProvider>
-            <TodoContext.Consumer>
-                {({tasks,saveItem, completedTasks, filteredTasks, searchQuery, setSearchQuery, isLoading, hasError}) => (
-                    <React.Fragment>
-                        <TodoCounter tasksQuantity={filteredTasks.length}
-                                     completedTasks={completedTasks.length}></TodoCounter>
-                        <TodoSearch searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-                        {isLoading &&
-                            <CustomSpinner/>
-                        }
-                        {hasError && <ErrorMessage/>}
-                        {(!isLoading && tasks.length === 0 && !hasError) &&
-                            <CreateTaskMessage/>
-                        }
-                        {tasks.length > 0 &&
-                            <TodoList tasks={tasks} filteredTasks={filteredTasks} saveTasks={saveItem}/>
-                        }
-                        <CreateTodoButton tasks={tasks}></CreateTodoButton>
-                    </React.Fragment>)}
-            </TodoContext.Consumer>
-        </TodoProvider>
+        <React.Fragment>
+            <TodoCounter></TodoCounter>
+            <TodoSearch/>
+            {isLoading &&
+                <CustomSpinner/>
+            }
+            {hasError && <ErrorMessage/>}
+            {(!isLoading && tasks.length === 0 && !hasError) &&
+                <CreateTaskMessage/>
+            }
+            {tasks.length > 0 &&
+                <TodoList tasks={tasks} filteredTasks={filteredTasks} saveTasks={saveItem}/>
+            }
+            <CreateTodoButton tasks={tasks}></CreateTodoButton>
+        </React.Fragment>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './Components/App/App';
+import { TodoProvider } from "./Contexts/TodoContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<TodoProvider><App /></TodoProvider>);
