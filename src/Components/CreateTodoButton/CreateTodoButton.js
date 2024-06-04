71cd8a6c0f95bbb6271065567b292b9454940713
gru-erick <ermateze@gmail.com>
2024-06-04 09:09:51 -0400

    - Fixing to do button problem

--- src/Components/CreateTodoButton/CreateTodoButton.js
+++ src/Components/CreateTodoButton/CreateTodoButton.js
@@ -1,17 +1,10 @@
 import React from "react";
 
 function CreateTodoButton({items}) {
-    function createButton(){
-        const value = document.querySelector("#todo-search").value
-        console.log(items);
-        items.push({name: value});
-    }
     return(
-        <div className={"fixed-bottom text-end"}>
-            <button className={"add-button"} onClick={createButton}>
-                <i className={"fas fa-plus-circle icon-color"} ></i>
-            </button>
-        </div>
+        <button className={"add-button"}>
+            <i className={"fas fa-plus-circle icon-color"} ></i>
+        </button>
     )
 }
 
