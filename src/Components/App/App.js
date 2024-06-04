604b156b89c292cba1d0f94ecf7e0f9c4a45ab4a
gru-erick <ermateze@gmail.com>
2024-06-04 15:47:49 -0400

    - Adding new TodoForm to App.js - Importing Modal Styles fixing background color bug. - Todo form can store a new Task.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,6 +11,7 @@ import Modal from "../../Elements/Modal/Modal";
 import {TodoContext, TodoProvider} from "../../Contexts/TodoContext";
 import {AuthContext} from "../../Contexts/AuthContext";
 import { EmojiList } from "../../Variables/EmojiList";
+import TodoForm from "../TodoForm/TodoForm";
 
 function App() {
     const { tasks,saveItem, filteredTasks, isLoading, hasError, showModal, setShowModal} = useContext(TodoContext);
@@ -35,39 +36,8 @@ function App() {
             {showModal && (
                 <Modal>
                         <div className="modal-dialog modal-dialog-centered modal-lg">
-                            <div className="modal-content bg-secondary-subtle">
-
-                                <div className="modal-body">
-                                    <h4 className={"modal-title mb-5 "}>Create New Task</h4>
-                                    <form>
-                                        <div className="row">
-                                            <div className="col-md-10 col-12">
-                                                <div className={"mx-2"}>
-                                                    <label htmlFor="title" className={"form-label"}>Title</label>
-                                                    <textarea type="text" className={"form-control"} rows={4} id={"title"}/>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-2 col-12">
-                                                <div className={"mx-2"}>
-                                                    <label htmlFor="emoji" className={"form-label"}>Emoji</label>
-                                                    <select name="emoji" id="emoji" className={"form-select"}>
-                                                        {EmojiList.map((emoji: string) => {
-                                                            return <option value={emoji}>{emoji}</option>;
-                                                        })}
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    </form>
-                                </div>
-                                <div className="modal-footer justify-content-between">
-                                    <button className="btn btn-outline-dark" onClick={() => {
-                                        setShowModal(!showModal)
-                                    }}>Cerrar
-                                    </button>
-                                    <button className="btn btn-dark">Aceptar</button>
-                                </div>
+                            <div className="modal-content bg-secondary">
+                                <TodoForm></TodoForm>
                             </div>
                         </div>
                 </Modal>
