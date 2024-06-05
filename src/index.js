eff2c4d9724a96744e3407bc0535101327916463
gru-erick <ermateze@gmail.com>
2024-06-05 09:58:39 -0400

    Dowgrading version of react from 18 to 17.0.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import {render} from "react-dom";
 import './index.scss';
 import App from './Components/App/App';
 import { TodoProvider } from "./Contexts/TodoContext";
 import {AuthProvider} from "./Contexts/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AuthProvider><TodoProvider><App /></TodoProvider></AuthProvider>);
+const root = document.getElementById("root");
+render(<AuthProvider><TodoProvider><App /></TodoProvider></AuthProvider>, root)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<AuthProvider><TodoProvider><App /></TodoProvider></AuthProvider>);
